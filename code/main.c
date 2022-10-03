c1df0105020c0996ab4f5d529dbf4011d1e403b4
amirhnajafiz <najafizadeh21@gmail.com>
2022-10-03 09:49:31 +0330

    update: base code

--- code/main.c
+++ code/main.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
 int main() {
+  // creating an array of size 10.
+  int memory[10];
+
+  // now trying to access the 11th element.
+  printf("%d\n", memory[10]);
+
+  // let's see what happens if we update the 11th element.
+  memory[10] = 10;
+
+  // let's see what happens if we access the 11th element again.
+  printf("%d\n", memory[10]);
+
   return 0; 
 }
