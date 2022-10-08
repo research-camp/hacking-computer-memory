71790193fab9d2207a06880c643f7d33e5fe9931
amirhnajafiz <najafizadeh21@gmail.com>
2022-10-08 11:28:05 +0330

    update: code example

--- README.md
+++ README.md
@@ -15,7 +15,23 @@ memory parts out of array allocated space in memory.
 
 ```C
 // let's see what happens if we update the 11th element.
-memory[10] = 10;
+#include <stdio.h>
+
+int main() {
+  // creating an array of size 10.
+  int memory[10];
+
+  // now trying to access the 11th element.
+  printf("%d\n", memory[10]);
+
+  // let's see what happens if we update the 11th element.
+  memory[10] = 10;
+
+  // let's see what happens if we access the 11th element again.
+  printf("%d\n", memory[10]);
+
+  return 0; 
+}
 ```
 
 The program will execute without any errors or warnings. Allow us to access any element out of array allocated space.
--- code/main.c
+++ code/main.c
@@ -1,17 +1,31 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+ 
+int main(int argc, char *argv[])
+{
+  // Reserve 5 byte of buffer plus the terminating NULL.
+  // should allocate 8 bytes = 2 double words,
+  // To overflow, need more than 8 bytes...
+  char buffer[5]; // If more than 8 characters input
+                  // by user, there will be access
+                  // violation, segmentation fault
 
-int main() {
-  // creating an array of size 10.
-  int memory[10];
+  // a prompt how to execute the program...
+  if (argc < 2)
+  {
+        printf("strcpy() NOT executed....\n");
+        printf("Syntax: %s <characters>\n", argv[0]);
+        exit(0);
+  }
 
-  // now trying to access the 11th element.
-  printf("%d\n", memory[10]);
+  // copy the user input to mybuffer, without any
+  // bound checking a secure version is strcpy_s()
+  strcpy(buffer, argv[1]);
+  printf("buffer content= %s\n", buffer);
 
-  // let's see what happens if we update the 11th element.
-  memory[10] = 10;
+  // you may want to try strcpy_s()
+  printf("strcpy() executed...\n");
 
-  // let's see what happens if we access the 11th element again.
-  printf("%d\n", memory[10]);
-
-  return 0; 
+  return 0;
 }
